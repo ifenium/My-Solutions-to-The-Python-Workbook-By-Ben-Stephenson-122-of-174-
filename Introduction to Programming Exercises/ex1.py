38652120ed29e6da0dd5567f52a2e22e75fe784e
Ife Oyelakin <40589517+ifeoyelakin@users.noreply.github.com>
2020-05-10 15:51:50 +0100

    Update ex1.py

--- Introduction to Programming Exercises/ex1.py
+++ Introduction to Programming Exercises/ex1.py
@@ -1,4 +1,4 @@
-''''
+'''
 Exercise 1: Mailing Address 
 Create a program that displays your name and complete mailing address formatted in
 the manner that you would usually see it on the outside of an envelope. Your program
